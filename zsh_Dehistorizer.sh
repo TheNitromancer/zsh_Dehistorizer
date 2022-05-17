9e4e2f6352af51d38dc62193401caf582146127e
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-17 14:02:54 +0200

    First steps into Shell Scripting

---
+++ zsh_Dehistorizer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+currentLines=$(grep -c '^' ~/.zsh_history)
+contrastor=""
+searchdex=""
+
+echo "Currently handling a grand total of: $currentLines lines. Please stand by..."
+while [$currentLines - $contrastor -gt 0]
+do
+	
+done
