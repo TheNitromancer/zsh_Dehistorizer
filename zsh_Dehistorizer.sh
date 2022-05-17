66f8fda6772bc97b9be19f68ae2f98f65642d704
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-17 15:24:15 +0200

    Functional line formater and remover of lines

--- zsh_Dehistorizer.sh
+++ zsh_Dehistorizer.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-currentLines=$(grep -c '^' ~/.zsh_history)
-contrastor=""
-searchdex=""
+history=./.zsh_history
+currentLines=$(grep -c '^' $history)
+wordtobesearched=""
+contrastor=1
+searchdex=0
 
 echo "Currently handling a grand total of: $currentLines lines. Please stand by..."
-while [$currentLines - $contrastor -gt 0]
-do
-	
-done
+#while [$currentLines - $contrastor -gt 0]
+#do
+	wordtobesearched=$(awk "NR==$currentLines - $contrastor" $history | cut -d ";" -f 2)
+	echo $wordtobesearched
+#done
+#sed "$currentLines d" $history #Currently successful at removing the last line. -i will change the history file.
+
