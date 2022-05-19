cf67a0e87aae7cfef308a3d6849955caf0d2e5d7
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-19 18:38:24 +0200

    Removed repeated line (ironic)

--- newDehistorizer.sh
+++ newDehistorizer.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-history=./.zsh_history
+history=./prueba
 temp=./temp_zsh_history
 newFile=./new_zsh_history
 
 currentLine=""
 currentCommand=""
+sedSafeLine=""
 
 numOlines=""
 counter=1
 
+# Define a function to process commands for characters that would mess with sed
+function sedSaver {
+	echo $1 | sed 's/\\/\\\\/g; s/\./\\\./g; s/\*/\\\*/g; s/\//\\\//g'
+}
+
 # Remove spureus lines, flip the file and store it into a temp
-# sed '/^:/!d' $history | tail -r | cat > temp_zsh_history 
-tail -r $history | cat > $temp
+sed '/^:/!d' $history | tail -r | cat > $temp
+
 # Store the amount of lines of the file
 numOlines=$(grep -c "^" $temp)
 
@@ -23,10 +29,11 @@ echo "" | cat > $newFile
 while [[ "$counter" -le "$numOlines" ]]
 do
 	currentLine=$(awk "NR==$counter" $temp)
-	currentWord=$(echo $currentLine | cut -d ";" -f2-)
-	if [[ $(grep -c ":0;$currentWord ºªºªº " $newFile) -eq 0 ]]
+	currentCommand=$(echo $currentLine | cut -d ";" -f2-)
+	if [[ $(grep -c ":0;$currentCommand ºªºªº " $newFile) -eq 0 ]]
 	then
-		sed -i '' "s/^/$currentLine ºªºªº /" $newFile
+		sedSafeLine=$(sedSaver "$currentLine")
+		sed -i '' "s/^/$sedSafeLine ºªºªº /" $newFile
 	fi
 	((counter += 1))
 done
@@ -39,4 +46,4 @@ sed -i '' 's/ ºªºªº /\
 sed -i '' "/^$/d" $newFile
 
 # Remove temp file
-rm $temp
+#rm $temp
