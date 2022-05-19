0f7fb98d85500770946ba697a7f117a47e48859b
Nestor Tamayo Franco <ntamayo-@student.42malaga.com>
2022-05-19 17:46:38 +0200

    Seemingly working Dehistorizer

--- newDehistorizer.sh
+++ newDehistorizer.sh
@@ -23,7 +23,7 @@
 
 history=./prueba
 temp=./temp_zsh_history
-newFile=./newHist
+newFile=./new_zsh_history
 
 currentLine=""
 currentCommand=""
@@ -33,22 +33,28 @@ counter=1
 
 # Remove spureus lines, flip the file and store it into a temp
 # sed '/^:/!d' $history | tail -r | cat > temp_zsh_history 
-cat $history > $temp
+tail -r $history | cat > $temp
 # Store the amount of lines of the file
 numOlines=$(grep -c "^" $temp)
 
 # Create the output file
-touch $newFile
+echo "" | cat > $newFile
 
 # Cicle each line
 while [[ "$counter" -le "$numOlines" ]]
 do
 	currentLine=$(awk "NR==$counter" $temp)
 	currentWord=$(echo $currentLine | cut -d ";" -f2-)
-	if [[ $(grep -c "$currentWord" $newFile) -eq 0 ]]
+	if [[ $(grep -c ":0;$currentWord ºªºªº " $newFile) -eq 0 ]]
 	then
-		echo "no ta $currentLine"
-		sed -i '' "1s/^/$currentLine/" $newFile
+		sed -i '' "s/^/$currentLine ºªºªº /" $newFile
 	fi
 	((counter += 1))
 done
+
+# Substitute the weird marker for a newline
+sed -i '' 's/ ºªºªº /\
+/g' $newFile
+
+# Delete empty lines
+sed -i '' "/^$/d" $newFile
